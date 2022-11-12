d80c703ed88773e9ca772387c5940cbde2176c0e
Wahyu Kristianto <w.kristories@gmail.com>
2022-11-12 15:28:57 +0700

    Setup lib

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use gjson;
+
+fn parse(search: &str) -> String {
+    let json = std::fs::read_to_string("./codes.json").unwrap();
+    let binding = gjson::get(&json, &search);
+
+    if !binding.exists() {
+        panic!("Status code or message does not exist!");
+    }
+
+    let value = binding.to_string();
+
+    return value;
+}
+
+pub fn code(code: &str) -> String {
+    let search = format!("#(message=={}).code", code);
+    let status = parse(&search);
+
+    return status;
+}
+
+pub fn message(message: &str) -> String {
+    let search = format!("#(code=={}).message", message);
+    let message = parse(&search);
+
+    return message;
+}
